61859259ba7b85d4279976bb110ba4b8e2329e2d
Daniel Stankewitz <daniel.stankewitz@gmail.com>
2019-06-24 16:43:17 +0200

    Fix

--- AfterEffectsPlugin/deploy.bat
+++ AfterEffectsPlugin/deploy.bat
@@ -1,3 +1,3 @@
 @echo off
 
-xcopy "%1" "C:\Program Files\Adobe\Adobe After Effects CC 2019\Support Files\Plug-ins\Format" /c /y
+xcopy "%1" "C:\Program Files\Adobe\Adobe After Effects CC 2019\Support Files\Plug-ins" /c /y
